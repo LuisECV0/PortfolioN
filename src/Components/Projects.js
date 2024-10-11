fc965cdf3eaae5e968cc9366955d588ca33b2aa4
Luiscv1719 <luiscv1719@gmail.com>
2024-10-11 00:00:42 -0500

    a

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,61 +1,62 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
+import { FaReact, FaHtml5, FaCss3Alt, FaJs, FaNodeJs } from 'react-icons/fa';
 //import { FaExternalLinkAlt } from 'react-icons/fa';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import './Projects.css';
 
 const projects = [
   {
-    title: 'Project - Hacienda Montalván',
-    description: 'Description for project 3',
+    title: 'Hacienda Montalván',
+    description: [<FaHtml5 color="#E34F26" />, <FaCss3Alt color="#1572B6" />, <FaJs color="#F7DF1E" />],
     imgUrl: 'https://i.ibb.co/Z23D6PC/landing-hacienda-min.png',
     gitUrl: 'https://github.com/LuisECV0/landing-hacienda-montalban',  
   },
   {
     title: 'Project - Clone Netflix',
-    description: 'Clon_Netflix_React',
+    description: [<FaHtml5 color="#E34F26" />, <FaCss3Alt color="#1572B6" />, <FaJs color="#F7DF1E" />],
     imgUrl: 'https://i.ibb.co/2hQS8H5/Clone-Netflix-min.png',
     gitUrl: 'https://github.com/LuisECV0/Clone-Netflix',
   },
   {
     title: 'Project - Clone Google',
-    description: 'Description for project 3',
+    description: [<FaHtml5 color="#E34F26" />, <FaCss3Alt color="#1572B6" />, <FaJs color="#F7DF1E" />],
     imgUrl: 'https://i.ibb.co/QdWLjCg/clone-google-min.png',
     gitUrl: 'https://github.com/LuisECV0/Google-Cln',
   }, 
   {
     title: 'Project - Ecommerce',
-    description: 'Description for project 2',
+    description: [<FaHtml5 color="#E34F26" />, <FaCss3Alt color="#1572B6" />, <FaJs color="#F7DF1E" />],
     imgUrl: 'https://i.ibb.co/bFHD5VL/Ecommerce-min.png',
     gitUrl: 'https://github.com/LuisECV0/projectEcommerce',
   },
   {
     title: 'Project - Calculator',
-    description: 'Description for project 3',
+    description: [<FaHtml5 color="#E34F26" />, <FaCss3Alt color="#1572B6" />, <FaJs color="#F7DF1E" />],
     imgUrl: 'https://i.ibb.co/pRCXygX/cal-sass-min.png',
     gitUrl: 'https://github.com/LuisECV0/calculadoraSAS',
   },
   {
     title: 'Project - FB Clone',
-    description: 'Clon_Google_Reac',
+    description: [<FaHtml5 color="#E34F26" />, <FaCss3Alt color="#1572B6" />, <FaJs color="#F7DF1E" />],
     imgUrl: 'https://i.ibb.co/HVyGrBS/clon-FB-min.png',
     gitUrl: 'https://github.com/LuisECV0/clonFB',
   },   
   {
-    title: 'Project - Rock, Paper, Scissors',
-    description: 'Description for project 3',
+    title: 'Rock, Paper, Scissors',
+    ddescription: [<FaHtml5 color="#E34F26" />, <FaCss3Alt color="#1572B6" />, <FaJs color="#F7DF1E" />],
     imgUrl: 'https://i.ibb.co/hWVrMr1/p1-min.png',
     gitUrl: 'https://github.com/LuisECV0/Piedra-papel-tijera',
   },
   {
     title: 'LANDNG PAGE RE ZERO',
-    description: 'Description for project 3',
+    description: [<FaHtml5 color="#E34F26" />, <FaCss3Alt color="#1572B6" />, <FaJs color="#F7DF1E" />],
     imgUrl: 'https://i.ibb.co/GPxCHRX/p2-min.png',
     gitUrl: 'https://github.com/LuisECV0/landing-page',
   },  
   {
     title: 'Project - Login',
-    description: 'Description for project 3',
+    description: [<FaHtml5 color="#E34F26" />, <FaCss3Alt color="#1572B6" />, <FaJs color="#F7DF1E" />],
     imgUrl: 'https://i.ibb.co/KxP5FXQ/p4-min.png',
     gitUrl: 'https://github.com/LuisECV0/login-php',
   },  
