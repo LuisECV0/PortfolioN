8cc282376b447a7f00d4c10cf7bda7243e1d9c50
Luiscv1719 <141294557+LuisECV0@users.noreply.github.com>
2024-12-28 00:21:31 -0500

    se comento la parte de contacto para ubicar la fuente de la alerta

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { NavBar } from './Components/NavBar';
 import { Footer } from './Components/Footer';
 import './App.css';
 
-// Cargar componentes de manera perezosa
 const AboutMe = lazy(() => import('./Components/AboutMe').then(module => ({ default: module.AboutMe })));
 const Projects = lazy(() => import('./Components/Projects'));
 const Skills = lazy(() => import('./Components/Skills'));
@@ -17,7 +16,7 @@ function App() {
         <AboutMe />
         <Projects />
         <Skills />
-        <Contact />
+      {/**   <Contact />*/} 
       </Suspense>
       <Footer />
     </div>
