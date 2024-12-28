d6f2f61e2d8f4c047244065669e604fa7b7128a0
Luiscv1719 <141294557+LuisECV0@users.noreply.github.com>
2024-12-28 00:51:55 -0500

    prueba 3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 const AboutMe = lazy(() => import('./Components/AboutMe').then(module => ({ default: module.AboutMe })));
 const Projects = lazy(() => import('./Components/Projects'));
 const Skills = lazy(() => import('./Components/Skills'));
-const Contact = lazy(() => import('./Components/Contact'));
+
 
 function App() {
   return (
@@ -14,9 +14,9 @@ function App() {
       <NavBar />
       <Suspense fallback={<div>Loading...</div>}>
         <AboutMe />
-      
+        <Projects />
         <Skills />
-        <Contact />
+     
       </Suspense>
       <Footer />
     </div>
